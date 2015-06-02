2c0806215fd0745ab9dcd54fa7ab93de41f9a7b4
AnasGhrab <anas.ghrab@gmail.com>
2015-06-02 23:36:41 +0100

    v. 0.0.3

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ def readme():
         return f.read()
 
 setup(name='diastema',
-      version='0.0.2',
+      version='0.0.3',
       description='A tool for modal music analysis : Scale analysis, tonic detection',
       long_description=readme(),
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Topic :: Musicology :: Analysis',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
       ],
       keywords='musicology analysis scale pitch makam melody',
       url='https://github.com/AnasGhrab/diastema',
